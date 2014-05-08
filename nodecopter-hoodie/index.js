ffe59b3a5b83b5b575f5eb6c76751a25329fa691
Andrew Nesbitt <andrew.nesbitt@github.com>
2014-05-08 12:49:45 +0100

    Visual hoodie arms

--- nodecopter-hoodie/index.js
+++ nodecopter-hoodie/index.js
@@ -2,13 +2,42 @@ var five = require("johnny-five"),
     arDrone = require('ar-drone'),
     client  = arDrone.createClient();
 
+var express = require('express');
+var ejs = require ('ejs')
+var app = express();
+
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.set('dirname', __dirname);
+  app.use(app.router);
+  app.use(express["static"](__dirname + "/public/"));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  return app.set('views', __dirname + "/views/");
+});
+
+app.get("/", function(req, res) {
+  return res.render('index.ejs');
+});
+
+var port = process.env.PORT || 8000;
+app.listen(port);
+console.log("Listening on Port '" + port + "'");
+
+var io = require('socket.io').listen(8001);
+
 five.Board().on("ready", function(){
   var leftFlexSensor = new five.Sensor("A0");
-
-  // client.takeoff();
+  var rightFlexSensor = new five.Sensor("A1");
 
   leftFlexSensor.on("read", function(err, value){
-    console.log(value)
-    // go left or right etc
+    var a= five.Fn.map(value, 150, 500, -90, 90)
+    io.sockets.emit('left', { angle: a, value: value });
+  });
+  rightFlexSensor.on("read", function(err, value){
+    var a = five.Fn.map(value, 500, 150, -90, 90)
+    io.sockets.emit('right', { angle: a, value: value });
   });
 });
