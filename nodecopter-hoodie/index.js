b479e10179b9bf34668f1123dfe838ab0f30fe06
Andrew Nesbitt <andrew.nesbitt@github.com>
2014-05-10 14:00:41 +0100

    Both arms with nodecopter commands setup

--- nodecopter-hoodie/index.js
+++ nodecopter-hoodie/index.js
@@ -28,16 +28,52 @@ console.log("Listening on Port '" + port + "'");
 
 var io = require('socket.io').listen(8001);
 
+var leftVertical = 0;
+var rightVertical = 0;
+
+var upperLimit = 30;
+var lowerLimit = -30;
+
+function move() {
+  var left = leftVertical
+  var right = rightVertical*-1
+  // up
+  if(left > upperLimit && right > upperLimit){
+    command = 'up'
+  }
+  // down
+  else if(left < lowerLimit && right < lowerLimit){
+    command = 'down'
+  }
+  // left
+  else if(left < lowerLimit && right > upperLimit){
+    command = 'left'
+  }
+  // right
+  else if(left > upperLimit && right < lowerLimit){
+    command = 'right'
+  }
+  // hover
+  else {
+    command = 'hover'
+  }
+  io.sockets.emit('command', {command: command, left: left, right: right});
+}
+
 five.Board().on("ready", function(){
   var leftFlexSensor = new five.Sensor("A0");
   var rightFlexSensor = new five.Sensor("A1");
 
   leftFlexSensor.on("read", function(err, value){
-    var a= five.Fn.map(value, 150, 500, -90, 90)
-    io.sockets.emit('left', { angle: a, value: value });
+    var a= five.Fn.map(value, 100, 400, -90, 90)
+    leftVertical = five.Fn.constrain(a, -80, 80)
+    io.sockets.emit('left', { angle: leftVertical, value: value });
+    move()
   });
   rightFlexSensor.on("read", function(err, value){
-    var a = five.Fn.map(value, 500, 150, -90, 90)
-    io.sockets.emit('right', { angle: a, value: value });
+    var a = five.Fn.map(value, 500, 100, -90, 90)
+    rightVertical = five.Fn.constrain(a, -80, 80)
+    io.sockets.emit('right', { angle: rightVertical, value: value });
+    move()
   });
 });
