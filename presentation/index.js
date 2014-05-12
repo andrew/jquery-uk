0d50c1752189fca878dae28757bb45eb93147dd5
Andrew Nesbitt <andrew.nesbitt@github.com>
2014-05-12 15:11:59 +0100

    Expressified presentation

---
+++ presentation/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var ejs = require ('ejs')
+var app = express();
+
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.set('dirname', __dirname);
+  app.use(app.router);
+  app.use(express["static"](__dirname + "/public/"));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  return app.set('views', __dirname + "/views/");
+});
+
+app.get("/", function(req, res) {
+  return res.render('index.ejs');
+});
+
+var port = process.env.PORT || 7001;
+app.listen(port);
+console.log("Listening on Port '" + port + "'");
