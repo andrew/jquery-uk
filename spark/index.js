6151cb31146d80de6a88018bd78e3a8c356b18f9
Andrew Nesbitt <andrew.nesbitt@github.com>
2014-05-15 14:18:59 +0100

    Tweet streaming

--- spark/index.js
+++ spark/index.js
@@ -1,24 +1,80 @@
 var Sparky = require('sparky')
 
-var core1 = new Sparky({
-    deviceId: '50ff6b065067545622150387',
-    token: '9d1342f907685e3f371c3f79efed2f7bbd173f2d',
+// var core1 = new Sparky({
+//     deviceId: '50ff6b065067545622150387',
+//     token: '9d1342f907685e3f371c3f79efed2f7bbd173f2d',
+// })
+
+var Twit = require('twit')
+
+var T = new Twit({
+    consumer_key:         'fgO7FlHNn734ulAMuDUCZOoH2'
+  , consumer_secret:      '0YWo6yWD9o3Osx9NxLQkhJvemHd6lnWJJdx5NjhH2ClAWsOnXI'
+  , access_token:         '686803-SckzWhXA2wAsHfOKmMAqnD13TstssPCVA0ohCHe7Cyd'
+  , access_token_secret:  'c6dtSJuYRXUUtBTNtmX7WEFi2cwb8UQTSnJQJ9hm5LIW1'
 })
 
-core1.run('ledon', 'D0', function(e){
-  console.log(e)
-});
+var stream = T.stream('statuses/filter', { track: 'google' })
 
-setTimeout(function(){
-    core1.run('ledoff', 'D0', function(e){
-      console.log(e)
-    });
-}, 2000)
+var keyword = 'sparkhat' // try google for testing
+var count = 0;
+var max = 50;
+var LEDcount = 5;
+var range = max / LEDcount;
+var LEDs = ['D0', 'D1', 'D2', 'D3', 'D4']
 
-var i = 0
-setInterval(function() {
-    core1.run('servo', i, function(e,d){
-      console.log(e,d)
-    });
-    i+=1
-},100)
+turnOffLEDs()
+
+stream.on('tweet', updateCount)
+
+function updateCount(tweet) {
+  // push avatar and count over websocket
+  count += 1
+
+  // turn on portion of LEDs
+  activateLEDs()
+
+  // raise servo arm when we reach the goal
+  if(count>=max){
+    raiseServo()
+    flashLEDs()
+  }
+}
+
+function raiseServo() {
+  console.log('raise Servo')
+  // core1.run('servo', i, function(resp){});
+}
+
+function activateLEDs() {
+  inactive = Math.floor((max-count)/range)
+
+  current = LEDcount - inactive - 2
+
+  if(current > -1){
+    console.log('D' + current, 'on')
+    // core1.run('ledon', 'D' + current, function(e){});
+  }
+}
+
+function turnOffLEDs(){
+  LEDs.forEach(function (value, index) {
+    console.log(value, 'off')
+    // core1.run('ledoff', value, function(e){});
+  });
+}
+
+function flashLEDs(){
+  LEDs.forEach(function (value, index) {
+    setInterval(function() {
+      // on
+      console.log(value, 'on')
+      // core1.run('ledon', value, function(e){});
+      setTimeout(function(){
+        // off
+        console.log(value, 'off')
+        // core1.run('ledoff', value, function(e){});
+      }, 1000)
+    }, 2000)
+  });
+}
