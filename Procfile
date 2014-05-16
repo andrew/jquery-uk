6c747af0ba2c999264f9badc5310ee554d52bda3
Andrew Nesbitt <andrew.nesbitt@github.com>
2014-05-16 12:32:33 +0100

    Spark core demo

--- Procfile
+++ Procfile
@@ -2,4 +2,4 @@ presentation: node presentation/index.js
 slotmachine: node slot-machine/index.js
 nodecopter: node nodecopter-hoodie/index.js
 dronestream: node dronestream/index.js
-# hat: node hat/index.js
+spark: node spark/index.js
--- spark/index.js
+++ spark/index.js
@@ -14,16 +14,44 @@ var T = new Twit({
   , access_token_secret:  'c6dtSJuYRXUUtBTNtmX7WEFi2cwb8UQTSnJQJ9hm5LIW1'
 })
 
-var stream = T.stream('statuses/filter', { track: 'google' })
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
 
-var keyword = 'google' // try google for testing
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.set('dirname', __dirname);
+  app.use(app.router);
+  app.use(express["static"](__dirname + "/public/"));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  return app.set('views', __dirname + "/views/");
+});
+
+var keyword = 'jqukhat' // try google for testing
 var count = 0;
-var max = 100;
+var max = 40;
 var LEDcount = 5;
 var range = max / LEDcount;
 var activeLEDs = []
 var LEDs = ['D0', 'D1', 'D2', 'D3', 'D4']
 var servoRaised = false;
+
+var stream = T.stream('statuses/filter', { track: keyword })
+
+app.get("/", function(req, res) {
+  return res.render('index.ejs', {hashtag: keyword});
+});
+
+var port = process.env.PORT || 5300;
+app.listen(port);
+console.log("Listening on Port '" + port + "'");
+
+var io = require('socket.io').listen(8002);
+
+
 
 reset()
 
@@ -32,6 +60,8 @@ stream.on('tweet', updateCount)
 function updateCount(tweet) {
   // push avatar and count over websocket
   count += 1
+  console.log(count)
+  io.sockets.emit('count', count)
 
   // turn on portion of LEDs
   activateLEDs()
@@ -39,7 +69,7 @@ function updateCount(tweet) {
   // raise servo arm when we reach the goal
   if(count>=max && servoRaised === false){
     raiseServo()
-    flashLEDs()
+    // flashLEDs()
   }
 }
 
